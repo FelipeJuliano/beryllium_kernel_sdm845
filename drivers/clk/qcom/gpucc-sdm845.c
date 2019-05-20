30e239c43ed6feed8c270406cffab4c254f5a9fd
Felipe C. Juliano <225872017@eniac.edu.br>
2019-05-20 14:08:15 -0300

    GPU OC adreno 630

--- drivers/clk/qcom/gpucc-sdm845.c
+++ drivers/clk/qcom/gpucc-sdm845.c
@@ -275,13 +275,13 @@ static const struct freq_tbl ftbl_gpu_cc_gx_gfx3d_clk_src[] = {
 
 static const struct freq_tbl  ftbl_gpu_cc_gx_gfx3d_clk_src_sdm845_v2[] = {
 	F(180000000, P_CRC_DIV,  1, 0, 0),
-	F(257000000, P_CRC_DIV,  1, 0, 0),
-	F(342000000, P_CRC_DIV,  1, 0, 0),
-	F(414000000, P_CRC_DIV,  1, 0, 0),
-	F(520000000, P_CRC_DIV,  1, 0, 0),
-	F(596000000, P_CRC_DIV,  1, 0, 0),
-	F(675000000, P_CRC_DIV,  1, 0, 0),
-	F(710000000, P_CRC_DIV,  1, 0, 0),
+	F(267000000, P_CRC_DIV,  1, 0, 0),
+	F(362000000, P_CRC_DIV,  1, 0, 0),
+	F(424000000, P_CRC_DIV,  1, 0, 0),
+	F(530000000, P_CRC_DIV,  1, 0, 0),
+	F(610000000, P_CRC_DIV,  1, 0, 0),
+	F(695000000, P_CRC_DIV,  1, 0, 0),
+	F(770000000, P_CRC_DIV,  1, 0, 0),
 	{ }
 };
 
@@ -629,17 +629,17 @@ static void gpu_cc_gfx_sdm845_fixup_sdm845v2(void)
 				ftbl_gpu_cc_gx_gfx3d_clk_src_sdm845_v2;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_MIN] = 180000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_LOWER] =
-		257000000;
-	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_LOW] = 342000000;
+		267000000;
+	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_LOW] = 362000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_LOW_L1] =
-		414000000;
+		424000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_NOMINAL] =
-		520000000;
+		530000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_NOMINAL_L1] =
-		596000000;
-	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] = 675000000;
+		610000000;
+	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] = 695000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_L1] =
-		710000000;
+		770000000;
 }
 
 static void gpu_cc_gfx_sdm845_fixup_sdm670(void)
